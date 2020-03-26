b125e72943a6f2e190a35f7f5c756dcc6dde40b8
Jared S <jaredjames2020@gmail.com>
2020-03-26 17:57:19 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,3 +4,9 @@ append.push("Odie");
 
 let prepend = ["Milo", "Otis", "Garfield"];
 prepend.pop("Odie")
+
+let removeLast = ["Milo", "Otis", "Garfield"];
+removeLast.pop()
+
+let removeFirst = ["Milo", "Otis", "Garfield"];
+removeFirst.shift()
